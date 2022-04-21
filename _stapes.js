ccbd16609c7aedd7e1242606ceb4d684128cf136
prokash <pprokash102@gmail.com>
2022-04-22 01:05:41 +0600

    server site create mogngDB node with express js

---
+++ _stapes.js
@@ -0,0 +1,18 @@
+/**
+ * npm init (set up node.js packages)
+ * npm install express
+ * npm i cors mongodb dotenv
+ * to to package.json and  set scripts > start node index.js and "start-dev": "nodemon index.js"
+ * cors middleware set up allow to connect server site to clint site (app.use(cors()))
+ * port set up (const port = process.env.PORT || 5000;)
+ * require('dotenv').config() / secure data
+ * body data show to middleware setup (app.use(express.JSON()))
+ * create mongoDB database atlas and create data file and insert json data
+ * go to connect and copy code and past index.js
+ * create new user and .env(.env file create root folder) file (KEY_USER=username) and put this index.js mongoBD code path using template string
+ * create a async function run and return run()
+ * try{}finally{} set and try into set await clint.connection() then const serviceCollection = client.db("carservice").collection("services");
+ * apply get function and set data link and send data clint site
+ * fetch data clint site and show display data
+ * post data apply app.post("/file",(req,res)) use req.body
+ */
---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cbjoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("carservice").collection("services");
+    // all service data
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const service = await cursor.toArray();
+      res.send(service);
+    });
+    // single data
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+    // post data
+    app.post("/service", async (req, res) => {
+      const newService = req.body;
+      const service = await serviceCollection.insertOne(newService);
+      res.send(service);
+    });
+    //delete data
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+// get data
+app.get("/users", (req, res) => {
+  res.send("Hello world");
+});
+
+//read data
+app.listen(port, () => {
+  console.log("my port ", port);
+});

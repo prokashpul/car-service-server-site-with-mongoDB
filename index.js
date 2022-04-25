2596d5e853a3e0870eb3193568f801cb7b9e96a4
prokash <pprokash102@gmail.com>
2022-04-25 07:10:21 +0600

    access token crereate api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -20,6 +21,7 @@ async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("carservice").collection("services");
+    const orderCollection = client.db("carservice").collection("order");
     // all service data
     app.get("/service", async (req, res) => {
       const query = {};
@@ -46,6 +48,28 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const result = await serviceCollection.deleteOne(query);
       res.send(result);
+    });
+    //order get api
+    app.get("/order", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = orderCollection.find(query);
+      const order = await cursor.toArray();
+      res.send(order);
+    });
+    // order api create
+    app.post("/order", async (req, res) => {
+      const query = req.body;
+      const cursor = await orderCollection.insertOne(query);
+      res.send(cursor);
+    });
+    // create token access api
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.SECRET_TOKEN, {
+        expiresIn: "1d",
+      });
+      res.send(accessToken);
     });
   } finally {
   }
